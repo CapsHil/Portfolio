11d85f31a0f9ad1647efda73f6e97b79c6f40a15
Pierre RABY <Zenika@Pierres-MacBook-Pro.local>
2017-05-30 11:14:50 +0200

    Add images and email form

--- js/scripts.js
+++ js/scripts.js
@@ -110,7 +110,7 @@
 		
         $(window).load(function(){
         $(".typing").typed({
-            strings: ["I am in apprenticeship", "I am a Software Architect.", "I am a Java Developper.", "I am from ESIEA."],    /* You can change the home section typing text from
+            strings: ["I am in apprenticeship.", "I am a Software Architect.", "I am a Java Developper.", "I am the President."],    /* You can change the home section typing text from
 	                                                                                            here and do not use "&" use "and" */
             typeSpeed: 50
           });
